9b157111daa7ade0c6440c3b71f09dab61def360
Mubashra <bsdsf22m022@pucit.edu.pk>
2024-10-26 22:15:16 +0500

    Added Executor header file

---
+++ executor.h
@@ -0,0 +1,8 @@
+// executor.h
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+
+int execute(char* arglist[]);
+
+#endif
+
---
+++ prompt.h
@@ -0,0 +1,8 @@
+// prompt.h
+#ifndef PROMPT_H
+#define PROMPT_H
+
+char* generate_prompt();
+
+#endif
+
